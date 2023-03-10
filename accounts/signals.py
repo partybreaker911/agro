1dc5ad3d01b10304380fbe135818c1247ee0532c
To Kirill <tio.kirill@gmail.com>
2023-03-10 11:43:56 +0200

    refactor signals for users

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,35 +8,17 @@ User = get_user_model()
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_user_related_data(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
-
-@receiver(post_save, sender=User)
-def create_user_wallet(sender, instance, created, **kwargs):
-    if created:
         Wallet.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_wallet(sender, instance, **kwargs):
-    instance.wallet.save()
-
-
-@receiver(post_save, sender=User)
-def create_user_location(sender, instance, created, **kwargs):
-    if created:
         UserLocation.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def save_user_location(sender, instance, **kwargs):
+def save_user_related_data(sender, instance, **kwargs):
+    instance.profile.save()
+    instance.wallet.save()
     instance.userlocation.save()
 
 
